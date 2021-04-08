6554170dfec1eabf31f4a2fe705372f28ca3ee87
jbofill10 <juan.bofill10@gmail.com>
2021-04-08 01:34:49 -0400

    spring cleaning

--- app/app.py
+++ app/app.py
@@ -4,12 +4,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from website_util import util
+from user_settings import profile
 
 import os, pickle, requests, json, sys
 
 
 def main():
-    cnf = config()
+    profile.config()
 
     print('-_-_-_- WeebTool5000 -_-_-_-'.center(50))
 
@@ -82,26 +83,6 @@ def save_anime(anime):
     pickle.dump(data, f)
 
 
-def config():
-    if os.path.exists('user.cnf'):
-        f = open('user.cnf', 'rb')
-
-        cnf = pickle.load(f)
-
-        print(f'\nWelcome back, {cnf["name"]}\n')
-
-    else:
-        cnf = {}
-        f = open('user.cnf', 'wb')
-        user_name = input('Hello weeb, what is your name: ')
-        wsl = input('Are you are running this in wsl (n if you don\'t know)? (y/n):')
-        wsl = True if wsl == 'y' else False
-        cnf['name'] = user_name
-        cnf['wsl'] = wsl
-
-        pickle.dump(cnf, f)
-
-    return cnf
 
 
 if __name__ == '__main__':
---
+++ app/user_settings/profile.py
@@ -0,0 +1,22 @@
+import os, pickle
+
+def config():
+    if os.path.exists('user.cnf'):
+        f = open('user.cnf', 'rb')
+
+        cnf = pickle.load(f)
+
+        print(f'\nWelcome back, {cnf["name"]}\n')
+
+    else:
+        cnf = {}
+        f = open('user.cnf', 'wb')
+        user_name = input('Hello weeb, what is your name: ')
+        wsl = input('Are you are running this in wsl (n if you don\'t know)? (y/n):')
+        wsl = True if wsl == 'y' else False
+        cnf['name'] = user_name
+        cnf['wsl'] = wsl
+
+        pickle.dump(cnf, f)
+
+   
